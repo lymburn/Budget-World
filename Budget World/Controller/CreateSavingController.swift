02d388b0be61320b305bf27f64ffeead2a45047c
Eugene Lu <eugenelu9848@gmail.com>
2018-06-03 13:22:02 -0600

    Add navigation bar items and events

--- Budget World/Controller/CreateSavingController.swift
+++ Budget World/Controller/CreateSavingController.swift
@@ -7,11 +7,16 @@
 //
 
 import UIKit
+import SlideMenuControllerSwift
 
 class CreateSavingController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         setupViews()
+        navigationController?.navigationBar.titleTextAttributes = [NSAttributedStringKey.foregroundColor: UIColor.white]
+        navigationController?.navigationBar.barTintColor = UIColor.rgb(red: 15, green: 52, blue: 67)
+        navigationItem.title = "Create Saving"
+        setupBarItems()
     }
     
     let createSavingView: CreateSavingView = {
@@ -24,6 +29,7 @@ class CreateSavingController: UIViewController {
 //MARK: Setup
 extension CreateSavingController {
     fileprivate func setupViews() {
+        view.backgroundColor = .white
         view.addSubview(createSavingView)
         setupConstraints()
     }
@@ -33,5 +39,30 @@ extension CreateSavingController {
         createSavingView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
         createSavingView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true
         createSavingView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor).isActive = true
+    }
+    
+    fileprivate func setupBarItems() {
+        let cancelItem = UIBarButtonItem(title: "Cancel", style: .plain, target: self, action: #selector(cancelButtonPressed))
+        cancelItem.tintColor = UIColor.white
+        navigationItem.leftBarButtonItem = cancelItem
+        
+        let doneItem = UIBarButtonItem(title: "Done", style: .plain, target: self, action: #selector(doneButtonPressed))
+        doneItem.tintColor = UIColor.white
+        navigationItem.rightBarButtonItem = doneItem
+    }
+}
+
+//MARK: Touch events
+extension CreateSavingController {
+    @objc func cancelButtonPressed() {
+        let savingsController = SlideMenuController(mainViewController: SavingsController(), leftMenuViewController: SlideOptionsController())
+        savingsController.modalTransitionStyle = UIModalTransitionStyle.crossDissolve
+        present(savingsController, animated: true, completion: nil)
+    }
+    
+    @objc func doneButtonPressed() {
+        let savingsController = SlideMenuController(mainViewController: SavingsController(), leftMenuViewController: SlideOptionsController())
+        savingsController.modalTransitionStyle = UIModalTransitionStyle.crossDissolve
+        present(savingsController, animated: true, completion: nil)
     }
 }
